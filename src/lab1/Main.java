ae0663c9f6f7203ec767c9291dab52f6842b862b
user <user@EF01>
2020-09-29 09:41:02 +0800

    中文名字改成英文名字

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ³¯«Å¾§");
+		System.out.println("I'm CHEN HSUAN JU");
 		System.out.println("My nid is D0641962");
 		System.out.println("This is lab 1 hw");
 	}
