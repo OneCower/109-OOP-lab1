be59edf6445f6ab2f0660ffeaf25a7afb5d4fd40
user <user@EF01>
2020-09-29 09:36:06 +0800

    物件導向設計實習Lab1

---
+++ src/lab1/Main.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm ³¯«Å¾§");
+		System.out.println("My nid is D0641962");
+		System.out.println("This is lab 1 hw");
+	}
+
+}
